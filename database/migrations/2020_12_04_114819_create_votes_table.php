9bc542e2cfe8fc8bd644cc9efd4df6b4d6defbec
ishuacharis <ishuacharis@gmail.com>
2020-12-04 14:25:27 +0100

    created migrations for users housemates plaforms votes

--- database/migrations/2020_12_04_114819_create_votes_table.php
+++ database/migrations/2020_12_04_114819_create_votes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_no')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId("user_id")->constrained()->onDelete('cascade');
+            $table->foreignId("housemate_id")->constrained()->onDelete('cascade');
+            $table->foreignId('platform_id')->constrained();
+            $table->string('amount');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('votes');
     }
 }
