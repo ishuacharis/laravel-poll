e5b11b770575fa6b8c5f967f689336e857d3c996
ishuacharis <ishuacharis@gmail.com>
2021-01-25 21:06:21 +0100

    created custom middle to report error in json format

---
+++ app/Http/Middleware/CustomEnsureEmailIsVerified.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Redirect;
+
+class CustomEnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response  = [
+                'response' => [
+                'message' => 'Your email address is not verified.'
+            ]
+        ];
+        if (! $request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+            ! $request->user()->hasVerifiedEmail())) {
+            return $request->expectsJson()
+                ? response($response,403)
+                : Redirect::route('verification.notice');
+        }
+
+        return $next($request);
+    }
+
+    
+}
+
